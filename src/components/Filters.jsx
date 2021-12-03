987e600f98b98cfb7651117ed240fcdce765e7d4
Damian Filipkiewicz <d.filipkiewicz@qarbon.pl>
2021-12-03 18:01:36 +0100

    Added text filter input to component Added functionality that allows to use more filters fields

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,29 +1,43 @@
-import React, {useEffect, useRef, useState} from 'react';
-import {TextInput} from "./TextInput";
-import useDebounce from "../hooks/useDebounce";
+import React, { useState } from 'react';
+import { Button } from './Button';
+import { TextFilterInput } from "./TextInput";
 
 export const Filters = ({setFilters}) => {
 
-  const [filterName, setFilterName] = useState([])
-  const debouncedValue = useDebounce(filterName, 500)
-  const didMountRef = useRef(false)
+  const [filterNameProduct, setFilterNameProduct] = useState([])
+  const [filterPriceFrom, setFilterPriceFrom] = useState([])
+  const [filterPriceTo, setFilterPriceTo] = useState([])
+  const [filterCostFrom, setFilterCostFrom] = useState([])
+  const [filterCostTo, setFilterCostTo] = useState([])
 
-  const handleFilterInput = (value, filter_name) => {
-    if (!value) return setFilterName([])
-    setFilterName([[ filter_name, "contains", value ]])
+
+  const handleFilterInput = (value, filter_name, option, setFunction) => {
+    setFunction([filter_name, option, value])
   }
 
-  useEffect(() => {
-    if (!didMountRef.current) {
-      didMountRef.current = true
-    } else {
-      setFilters(prev => ({...prev, filter: filterName}))
+  const handleSearch = () => {
+    const filterArray = []
+    if (filterNameProduct.length && filterNameProduct[2]) filterArray.push(filterNameProduct)
+    if (filterPriceFrom.length && filterPriceFrom[2]) filterArray.push(filterPriceFrom)
+    if (filterPriceTo.length && filterPriceTo[2]) filterArray.push(filterPriceTo)
+    if (filterCostFrom.length && filterCostFrom[2]) filterArray.push(filterCostFrom)
+    if (filterCostTo.length && filterCostTo[2]) filterArray.push(filterCostTo)
+
+    for (let i = filterArray.length -1; i >= 1; i--){
+      filterArray.splice(i, 0, "and")
     }
-  }, [debouncedValue])
+    setFilters(prev => ({...prev, filter: filterArray}))
+
+  }
 
   return (
     <div className="filters">
-      <TextInput placeholder="Search for product" className="filters__input" inputName={'name.en'} onInput={handleFilterInput} />
+      <TextFilterInput placeholder="Search for product" className="filters__input" inputName={'name.en'} filterType={'contains'} filterSetFunction={setFilterNameProduct} onInput={handleFilterInput} />
+      <TextFilterInput type="number" placeholder="Price from" className="filters__input" inputName={'price'} filterType={'>='} filterSetFunction={setFilterPriceFrom} onInput={handleFilterInput} />
+      <TextFilterInput type="number" placeholder="Price to" className="filters__input" inputName={'price'} filterType={'<='} filterSetFunction={setFilterPriceTo} onInput={handleFilterInput} />
+      <TextFilterInput type="number" placeholder="Cost from" className="filters__input" inputName={'cost'} filterType={'>='} filterSetFunction={setFilterCostFrom} onInput={handleFilterInput} />
+      <TextFilterInput type="number" placeholder="Cost to" className="filters__input" inputName={'cost'} filterType={'<='} filterSetFunction={setFilterCostTo} onInput={handleFilterInput} />
+      <Button onClick={handleSearch} label="Search"/>
     </div>
   )
 }
--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,3 +1,7 @@
 export const TextInput = ({ inputName, ...props }) => {
   return <input {...props} onInput={(event) => props.onInput(event.target.value, inputName || null)}/>
 }
+
+export const TextFilterInput = ({ inputName, filterType, filterSetFunction, ...props }) => {
+  return <input {...props} onInput={(event) => props.onInput(event.target.value, inputName || null, filterType, filterSetFunction)}/>
+}
